5a92baaf380a2fb658e03cde4363165e10f14fff
doesburg <petervandoesburg11@gmail.com>
2022-12-03 16:31:09 +0100

    tootltip 1

--- examples/wolf_sheep/wolf_sheep/server.py
+++ examples/wolf_sheep/wolf_sheep/server.py
@@ -14,7 +14,7 @@ def wolf_sheep_portrayal(agent):
         portrayal["Shape"] = "wolf_sheep/resources/wolf.png"
         # https://icons8.com/web-app/36821/German-Shepherd
         #portrayal["scale"] = 0.9
-        portrayal["Layer"] = 1
+        portrayal["Layer"] = 2
         portrayal["id"] = agent.unique_id # round(agent.energy, 1)
         portrayal["energy"] = round(agent.energy, 2)
         portrayal["text_color"] = "White"
@@ -34,7 +34,7 @@ def wolf_sheep_portrayal(agent):
             portrayal["Color"] = ["#84e184", "#adebad", "#d6f5d6"]
         portrayal["Shape"] = "rect"
         portrayal["Filled"] = "true"
-        portrayal["Layer"] = 0
+        portrayal["Layer"] = 2
         portrayal["w"] = 1
         portrayal["h"] = 1
 
