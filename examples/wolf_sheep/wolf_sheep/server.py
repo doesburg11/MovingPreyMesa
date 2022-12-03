9ea75a9c3dd85951d8a7a3392bcd4b3cd9381440
doesburg <petervandoesburg11@gmail.com>
2022-12-03 15:08:36 +0100

    tootltip 1

--- examples/wolf_sheep/wolf_sheep/server.py
+++ examples/wolf_sheep/wolf_sheep/server.py
@@ -22,7 +22,7 @@ def wolf_sheep_portrayal(agent):
         # https://icons8.com/web-app/36821/German-Shepherd
         portrayal["scale"] = 0.9
         portrayal["Layer"] = 2
-        portrayal["text"] = 1 # round(agent.energy, 1)
+        portrayal["text"] = agent.unique_id # round(agent.energy, 1)
         portrayal["text_color"] = "White"
 
     elif type(agent) is GrassPatch:
